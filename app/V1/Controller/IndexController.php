55b8aa7088140847278a8a36452f2c6fb7bd3c84
Paweł Balicki <pawel@kilab.pl>
2017-09-22 20:58:00 +0200

    Versioning

--- app/V1/Controller/IndexController.php
+++ app/V1/Controller/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\V1\Controller;
 
 use Kilab\Api\Controller;
 
--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ const BASE_DIR = __DIR__ . '/../';
 
 $loader = require BASE_DIR . 'vendor/autoload.php';
 
+// TODO: improve passing API version to config
+Kilab\Api\Config::setApiVersion($_SERVER);
+
 $errorHandler = new Kilab\Api\ErrorHandler();
 $request = new Kilab\Api\Request();
 

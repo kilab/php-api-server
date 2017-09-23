834dc6b07261fedca4cd01d7f8d024c8d48ae1d6
Paweł Balicki <pawel@kilab.pl>
2017-09-23 13:43:08 +0200

    Improved API versioning

--- public/index.php
+++ public/index.php
@@ -2,13 +2,22 @@
 
 const BASE_DIR = __DIR__ . '/../';
 
+$requestServerInfo = $_SERVER;
+
+// determine API version to load
+preg_match('/v\d/i', $requestServerInfo['PATH_INFO'], $matches);
+
+if (!empty($matches)) {
+    define('API_VERSION', strtolower($matches[0]));
+} else {
+    header('Location: /v1');
+}
+
 $loader = require BASE_DIR . 'vendor/autoload.php';
 
-// TODO: improve passing API version to config
-Kilab\Api\Config::setApiVersion($_SERVER);
-
 $errorHandler = new Kilab\Api\ErrorHandler();
-$request = new Kilab\Api\Request();
+$request = new Kilab\Api\Request($requestServerInfo);
 
+// run server
 $apiServer = new Kilab\Api\Server($request);
 $apiServer->run();
